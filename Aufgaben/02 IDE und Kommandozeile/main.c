07907d7e848b6c08baef71c9485a8392768eec16
Dennis Schulmeister-Zimolong <dennis@patk.org>
2024-04-16 03:35:59 +0200

    Erste Übungsaufgaben

---
+++ Aufgaben/02 IDE und Kommandozeile/main.c
@@ -0,0 +1,27 @@
+/***********************************************************************************************
+ * Aufgabe: IDE und Kommandozeile
+ * ==============================
+ * 
+ * Nachdem die ersten Gehversuche mit dem Online-Compiler erfolgreich waren, kopieren Sie nun
+ * den Quellcode von der vorherigen Aufgabe in diese Datei. Hier zur Erinnerung, was das
+ * Programm machen sollte:
+ *
+ * 1. Bei Programmstart fragt das Programm nach dem Namen des/der Benutzer*in:
+ *
+ *    ```txt
+ *    Wie heißen Sie? Dagobert Duck					← Benutzereingabe des Namens
+ *    ```
+ *
+ *    Bedenken Sie, dass der Name "Dagobert Duck" nirgendswo im Quellcode fest hinterlegt
+ *    ist, sondern stattdessen als Benutzereingabe auf der Konsole abgefragt wurde.
+ *
+ * 2. Anschlioeßend soll folgende, personalisierte Grußbotschaft ausgegeben werden:
+ *
+ *    ```txt
+ *    Guten Morgen.
+ *    Hallo Dagobert Duck.							← Verwendung des Namens im Programm
+ *    Schöner Tag heute, nicht?
+ *    ```
+ ***********************************************************************************************/
+
+// Quellcode der vorherigen Lösung hier
---
+++ Aufgaben/03 Fehlersuche/main.c
@@ -0,0 +1,22 @@
+/***********************************************************************************************
+ * Aufgabe: Fehlersuche
+ * ====================
+ * 
+ * Das folgende Programm lässt sich leider nicht kompilieren. Oder wie mein Vorgänger Prof. Dr.
+ * Michael Bauer sagte: Fehlerhafte Programme zu schreiben ist leicht. Dummerweise sollen wir
+ * aber Programme ohne Fehler schreiben. :-)
+ *
+ * Versuchen Sie das Programm zu kompilieren und schauen Sie sich Fehlermeldungen auf der
+ * Kommandozeile an. Können Sie erkennen, welche Zeilen betroffen sind? Versuchen Sie dann so
+ * lange die Fehler zu korrigieren, bis sich das Programm wieder bauen und starten lässt.
+ ***********************************************************************************************/
+
+/**
+ * Hauptfunktion des Programms.
+ */
+Int main(int argc, char **argv) {
+	return 0;
+
+	printf("Hallo Welt!\n")
+	return 0;
+}
