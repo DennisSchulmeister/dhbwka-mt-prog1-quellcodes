7b89dda21f8e1aedddfda15dbdf1c685745385e1
Dennis Schulmeister-Zimolong <dennis@patk.org>
2024-05-14 19:26:42 +0200

    Grundgerüst als Kopiervorlage umgebaut

--- Beispiele/01 Allgemeine Syntax/01 Minimales Grundgerüst/main.cpp
+++ Beispiele/01 Allgemeine Syntax/01 Minimales Grundgerüst/main.cpp
@@ -1,18 +1,22 @@
 /***********************************************************************************************
- * Beispiel: Minimales Grundgerüst
- * ===============================
- * 
- * Dies ist das einfachste Programm, dass Sie in C oder C++ schreiben können. Es beinhaltet
- * einfach eine leere `main`-Funktion, die das Betriebssystem beim Starten des Programms
- * aufrufen kann, die aber nichts macht. Alle unsere Programme werden dieser Vorlage folgen.
+ * NAME DES PROGRAMMS
+ * ==================
+ *
+ * BESCHREIBUNG
+ *
+ * DATUM, EIGENER NAME, E-MAIL
  ***********************************************************************************************/
 
-/**
- * Hauptfunktion des Programms. Diese wird vom Betriebssystem aufgerufen, um das Programm
- * zu starten. Die beiden Parameter `argc` und `argv` bekommen die sog. Kommandozeilenparameter
- * (Argumente) übergeben. Als Rückgabewert wird ein optionaler, numerischer Wert erwartet, von dem
- * jeder andere Wert außer null einen Fehler symbolisiert.
- */
+#include <iostream>     // std::cout, std::cin, ...
+#include <iomanip>      // std::setprecision
+#include <string>       // std::string, std::getline, ...
+#include <vector>       // std::vector
+#include <fstream>      // std::ifstream, std::ofstream, ...
+#include <cmath>        // sin, ...
+
+using namespace std;
+
 int main(int argc, char **argv) {
-    // Quellcode hier
+    // Programmcode hier
+    return 0;
 }
