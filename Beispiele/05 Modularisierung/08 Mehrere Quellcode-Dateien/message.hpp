6f2cffe85ae74ea9889dcab76466735bfa45ce13
Dennis Schulmeister-Zimolong <dennis@patk.org>
2024-06-11 11:24:25 +0200

    Mehrere Quelldateien: Kommentar

--- Beispiele/05 Modularisierung/08 Mehrere Quellcode-Dateien/message.hpp
+++ Beispiele/05 Modularisierung/08 Mehrere Quellcode-Dateien/message.hpp
@@ -2,6 +2,9 @@
  * Header: message.hpp
  * ===================
  * 
+ * Header-Datei zum Module message.o, das der Compiler aus der message.cpp generiert. Hier finden
+ * sich die öffentlichen (auch für andere Module nutzbaren) Deklarationen. Zur Vermeidung von
+ * Namenskonflikten befinden sich die Deklarationen in einem eigenen Namensraum (namespace).
  ***********************************************************************************************/
 #pragma once            // Ignorieren, wenn die Datei mehrfach eingebunden wird
 #include <string>       // std::string, std::getline, ...
